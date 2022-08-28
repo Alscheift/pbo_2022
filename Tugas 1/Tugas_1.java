9902ad535f725866bdb782bff5dc29e71812b424
Alscheift <alscheift@gmail.com>
2022-08-28 20:35:19 +0700

    28-08-2022

--- Tugas 1/Tugas_1.java
+++ Tugas 1/Tugas_1.java
@@ -1,19 +1,25 @@
-
-import java.util.Scanner;
-
 class Mahasiswa {
     public String NIM,  nama, jenis_kelamin, alamat;
-    public void isiData(){
-        //System.out.println("Masukkan NIM :");
-        //System.in.read(NIM);
+    public void isiData(String NIM, String nama,String jenis_kelamin, String alamat){
+         this.NIM = NIM;
+         this.nama = nama;
+         this.jenis_kelamin = jenis_kelamin;
+         this.alamat = alamat;
     }
     public void printData(){
-
+        System.out.println(
+            "NIM           : "+this.NIM+'\n'+
+            "Nama          : "+this.nama+'\n'+
+            "Jenis Kelamin : "+this.jenis_kelamin+'\n'+
+            "Alamat        : "+this.alamat+'\n'
+            ); 
     }
 
 }
 class Tugas_1 {
     public static void main(String[] args) {
-        System.out.println("Test"); 
+        Mahasiswa mhs = new Mahasiswa();
+        mhs.isiData("M0521007","Afif Nur Fauzi", "Laki-Laki", "127.0.0.1");
+        mhs.printData();
     }
 }
