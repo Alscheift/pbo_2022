67250de8f614ebdeaa6f68d7259a9326158d5853
Alscheift <alscheift@gmail.com>
2022-08-28 20:23:51 +0700

    -

--- Tugas 1/Tugas_1.java
+++ Tugas 1/Tugas_1.java
@@ -1,5 +1,17 @@
-// Your First Program
 
+import java.util.Scanner;
+
+class Mahasiswa {
+    public String NIM,  nama, jenis_kelamin, alamat;
+    public void isiData(){
+        //System.out.println("Masukkan NIM :");
+        //System.in.read(NIM);
+    }
+    public void printData(){
+
+    }
+
+}
 class Tugas_1 {
     public static void main(String[] args) {
         System.out.println("Test"); 
