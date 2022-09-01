75e00bda5462abd852b20bd345234bb50523a711
Alscheift <alscheift@gmail.com>
2022-09-01 14:49:14 +0700

    exploring long and int limit

---
+++ Testing/DataTypes.java
@@ -0,0 +1,19 @@
+package Testing;
+
+public class DataTypes {
+    public static void main(String args[]){
+        long a = 100000147483648L;
+        long aa = 100000147483648L;
+        int b = 2147483647;
+        System.
+        out.
+        println
+        (
+            a+
+            aa+
+            long.
+            class.
+            toString()
+            );
+    }
+}
