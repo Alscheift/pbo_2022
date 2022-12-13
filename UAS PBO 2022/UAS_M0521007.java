1ab8f2cc1c225002e37d6e41a08f54a4ca5b81e3
Alscheift <alscheift@gmail.com>
2022-12-13 15:18:12 +0700

    First commit UAS

---
+++ UAS PBO 2022/UAS_M0521007.java
@@ -0,0 +1,15 @@
+/**
+ * Pemrograman Berbasis Objek
+ * 
+ * Afif Nur Fauzi M0521007
+ * Kelas A | Angkatan 2021
+ */
+
+public class UAS_M0521007 {
+    public static void main(String[] args) {
+        return 0;
+    }
+}
+
+
+
