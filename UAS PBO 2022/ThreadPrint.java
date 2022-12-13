8cea9b66257170cfa57c5e9ba9a715a8823cb96b
Alscheift <alscheift@gmail.com>
2022-12-13 16:53:32 +0700

    ThreadPrint interrupted try catch

--- UAS PBO 2022/ThreadPrint.java
+++ UAS PBO 2022/ThreadPrint.java
@@ -9,6 +9,12 @@ public class ThreadPrint extends Thread{
     public void run() {
         for (Object object : objectToPrint) {
             System.out.println(object);
+            try {
+                this.wait(500);
+            } catch (InterruptedException e) {
+                System.out.println("!! Proses Terinterupsi !!");
+                e.printStackTrace();
+            }
         }
         super.run();
     }
