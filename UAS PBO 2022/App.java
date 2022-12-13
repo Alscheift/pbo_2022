c05c8b0eb8157b15a48d696a6fe0a4357bc524b5
Alscheift <alscheift@gmail.com>
2022-12-13 16:14:40 +0700

    4. Class App (tanpa multithread)

---
+++ UAS PBO 2022/App.java
@@ -0,0 +1,22 @@
+
+public class App {
+    public static void main(String[] args) {
+        PegawaiTetap [] arrayPegawaiTetap = new PegawaiTetap[5];
+        PegawaiHonorer [] arrayPegawaiHonorer = new PegawaiHonorer[5];
+
+        arrayPegawaiTetap[0] = new PegawaiTetap("Deni", "Jakarta", 5000011, 3500000);
+        arrayPegawaiTetap[1] = new PegawaiTetap("Adi", "Medan", 4500012, 3200000);
+        arrayPegawaiTetap[2] = new PegawaiTetap("Sarah", "Surabaya", 4700013, 2500000);
+        arrayPegawaiTetap[3] = new PegawaiTetap("Silvia", "Semarang", 5200014, 2300000);
+        arrayPegawaiTetap[4] = new PegawaiTetap("Rahmat", "Bali", 4600015, 3300000);
+
+        arrayPegawaiHonorer[0] = new PegawaiHonorer("Zaki", "Surakarta", 3500021);
+        arrayPegawaiHonorer[1] = new PegawaiHonorer("Agung", "Surakarta", 3600022);
+        arrayPegawaiHonorer[2] = new PegawaiHonorer("Rangga", "Semarang", 3800023);
+        arrayPegawaiHonorer[3] = new PegawaiHonorer("Rendi", "Semarang", 3900024);
+        arrayPegawaiHonorer[4] = new PegawaiHonorer("Aldi", "Surabaya", 4000025);
+
+
+         
+    }
+}
--- UAS PBO 2022/UAS_M0521007.java
+++
@@ -1,5 +0,0 @@
-public class UAS_M0521007 {
-    public static void main(String[] args) {
-        
-    }
-}
